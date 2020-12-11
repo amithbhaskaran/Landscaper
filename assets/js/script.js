0889a3a9c90d54be819d32b0fdf076bd721fbead
bamithprdxn <amithkumarb@prdxn.com>
2020-12-11 17:31:53 +0530

    js code added

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,25 +2,29 @@
 
 */
 
+$(document).ready(function () {
 
+    // adding click on list
+    $(".list").click(function () {
+        // console.log(this);
+        let value = $(this).attr("data-filter");
+        console.log(value);
 
+        if (value == "all") {
+            $(".image-list-li").show();
+        } else {
+            $(".image-list-li").not("." + value).hide();
+            $(".image-list-li").filter("." + value).show();
+            // console.log($(".image-list-li").not("." + value).hide());
+            // console.log($(".image-list-li").filter("." + value).show());
 
+        }
+    })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    // adding active class
+    $(".list").click(function () {
+        $(this).addClass("active").siblings().removeClass("active");
+    })
+})
 
 
